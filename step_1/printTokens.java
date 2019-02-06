234ebbba61eb45d95566de462bec968c82294815
Mike Pollard <mpollard@wolfssl.com>
2019-02-05 22:01:49 -0700

    even smaller source code

--- step_1/printTokens.java
+++ step_1/printTokens.java
@@ -4,9 +4,8 @@ public class printTokens
     public static void main( String[] args) throws Exception 
     {	
         little_grammarLexer lex = new little_grammarLexer(CharStreams.fromFileName(args[0]));
-        Vocabulary voc = lex.getVocabulary();
         for (Token tok = lex.nextToken(); tok.getType() != tok.EOF; tok = lex.nextToken()) {
-            System.out.println("Token Type: " + voc.getSymbolicName(tok.getType()));
+            System.out.println("Token Type: " + lex.getVocabulary().getSymbolicName(tok.getType()));
             System.out.println("Value: " + tok.getText());
         }
     }
