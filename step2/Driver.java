4c44b863614bcbe31c1ea99e847951de9dd88b9b
Mike Pollard <mpollard@wolfssl.com>
2019-02-25 00:27:47 -0700

    half working for input files, need to change grammar

--- step2/Driver.java
+++ step2/Driver.java
@@ -1,17 +1,14 @@
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
 public class Driver 
 {
     public static void main( String[] args) throws Exception 
     {	
-        // Create needed instances.
         little_grammarLexer lex = new little_grammarLexer(CharStreams.fromFileName(args[0]));
         CommonTokenStream tok = new CommonTokenStream(lex);
-        litte_grammarParser par = new little_grammarParser(tokens);
-
-
-        // Determine and print out result.
+        little_grammarParser par = new little_grammarParser(tok);
+        new ParseTreeWalker().walk(new little_grammarBaseListener(), par.program());
         String result = par.getNumberOfSyntaxErrors() > 0 ? "Not Accepted" : "Accepted";
         System.out.println(result);
-
     }
 }
---
+++ step2/README.md
@@ -0,0 +1,2 @@
+run ./Micro to run against all input files
+run ./Micro (inputfile) to run against a specific file.
