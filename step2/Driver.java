aaf7bfe6d2c25c38ab3a0c610b8844fb9ccc83cf
Mike Pollard <mpollard@wolfssl.com>
2019-02-27 15:41:58 -0700

    smaller source code

--- step2/Driver.java
+++ step2/Driver.java
@@ -5,8 +5,7 @@ public class Driver
     public static void main( String[] args) throws Exception 
     {	
         little_grammarLexer lex = new little_grammarLexer(CharStreams.fromFileName(args[0]));
-        CommonTokenStream tok = new CommonTokenStream(lex);
-        little_grammarParser par = new little_grammarParser(tok);
+        little_grammarParser par = new little_grammarParser(new CommonTokenStream(lex));
         new ParseTreeWalker().walk(new little_grammarBaseListener(), par.program());
         String result = par.getNumberOfSyntaxErrors() > 0 ? "Not accepted" : "Accepted";
         System.out.println(result);
