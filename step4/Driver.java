693f1a8302d2f3484b26360ffc3c614095fac83d
wizmariefa <mariecolee1@gmail.com>
2019-04-21 15:33:30 -0600

    adding base files from step 3

---
+++ step4/Driver.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+/**
+ * This class runs the input files with the generated antlr components to create
+ * and then print out the SymbolTable.
+ *
+ * @Author Michael Pollard, Ethan Malo, Marie Morin, Trent Baker
+ */
+public class Driver {
+    public static void main(String[] args) throws Exception {
+        little_grammarParser parser = new little_grammarParser(
+                new CommonTokenStream(new little_grammarLexer(CharStreams.fromFileName(args[0]))));
+        Listener listener = new Listener();
+        new ParseTreeWalker().walk(listener, parser.program());
+
+        for (SymbolTable s : listener.getSymbolTables()) {
+            if (!s.isValid()) {
+                System.out.println("DECLARATION ERROR n");
+                return;
+            }
+        }
+
+        for (SymbolTable s : listener.getSymbolTables()) {
+            s.prettyPrint();
+        }
+    }
+
+}
