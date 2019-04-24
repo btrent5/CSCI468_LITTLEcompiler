16537defce7515e77059f85223e6c9eab9b56c06
trent <b.trent5@gmail.com>
2019-04-24 14:17:28 -0600

    generating code, except for var decl at top of file...

--- step4/tinyGenerator.java
+++ step4/tinyGenerator.java
@@ -1,112 +1,120 @@
 import java.util.ArrayList;
 
 class TinyGenerator {
+	private int varCount = 0;
 
 	public TinyGenerator(ArrayList<IRNode> input) {
 		StringBuilder output = new StringBuilder();
 		for (IRNode current : input) {
+			// still need to prepend all var names maybe make part of convertRegString?? tbd
 			switch (current.getOperator()) {
 			case "ADDI":
-				output.append("addi\n");
+				output.append("move " + convertRegString(current.getReg1()) + " " + convertRegString(current.getReg3())
+						+ "\n");
+				output.append("addi " + convertRegString(current.getReg2()) + " " + convertRegString(current.getReg3())
+						+ "\n");
 				break;
 			case "ADDR":
-				output.append("addr\n");
 				break;
 			case "CMPI":
-				output.append("cmpi\n");
 				break;
 			case "CMPR":
-				output.append("cmpr\n");
 				break;
 			case "DECI":
-				output.append("deci\n");
 				break;
 			case "DIVI":
-				output.append("divi\n");
 				break;
 			case "DIVR":
-				output.append("divr\n");
 				break;
 			case "EMPTYLINE":
-				output.append("emptyline\n");
 				break;
 			case "ENDOFPROGRAM":
-				output.append("endofprogram\n");
+				break;
+			case "GEI":
+				output.append("cmpi " + current.getReg1() + " " + convertRegString(current.getReg2()) + "\n");
+				output.append("jge " + current.getReg3() + "\n");
 				break;
 			case "INCI":
-				output.append("inci\n");
 				break;
 			case "JEQ":
-				output.append("jeq\n");
 				break;
 			case "JGE":
-				output.append("jge\n");
 				break;
 			case "JGT":
-				output.append("jgt\n");
 				break;
 			case "JLE":
-				output.append("jle\n");
 				break;
 			case "JLT":
-				output.append("jlt\n");
 				break;
-			case "JMP":
-				output.append("jmp\n");
+			case "JUMP":
+				output.append("jmp " + current.getReg1() + "\n");
 				break;
 			case "JNE":
-				output.append("jne\n");
 				break;
 			case "JSR":
-				output.append("jsr\n");
 				break;
 			case "LABEL":
-				output.append("label\n");
+				output.append("label " + current.getReg1() + "\n");
 				break;
 			case "LINK":
-				output.append("link\n");
 				break;
 			case "MOVE":
-				output.append("move\n");
 				break;
 			case "MULI":
-				output.append("muli\n");
 				break;
 			case "MULR":
-				output.append("mulr\n");
 				break;
 			case "POP":
-				output.append("pop\n");
 				break;
 			case "PUSH":
-				output.append("push\n");
 				break;
 			case "RET":
-				output.append("ret\n");
+				output.append("sys halt");
 				break;
-			case "STR":
-				output.append("str\n");
+			case "STOREI":
+				output.append("move " + convertRegString(current.getReg1()) + " " + convertRegString(current.getReg2())
+						+ "\n");
 				break;
 			case "SUBI":
-				output.append("subi\n");
+				output.append("move " + convertRegString(current.getReg1()) + " " + convertRegString(current.getReg3())
+						+ "\n");
+				output.append("subi " + convertRegString(current.getReg2()) + " " + convertRegString(current.getReg3())
+						+ "\n");
 				break;
 			case "SUBR":
-				output.append("subr\n");
 				break;
 			case "SYS":
-				output.append("sys\n");
 				break;
 			case "UNKNOWN":
-				output.append("unknown\n");
 				break;
 			case "UNLNK":
-				output.append("unlnk\n");
 				break;
 			case "VAR":
-				output.append("var\n");
+				break;
+			case "WRITEI":
+				output.append("sys writei " + current.getReg1() + "\n");
+				// ;WRITEI num
+				// sys writei num
 				break;
 			}
 		}
 		System.out.println(output);
+	}
+
+	/*
+	 * takes an input and if it matches on /\$T[0-9]+/g then it is converted to the
+	 * form as shown in the test outputs
+	 */
+	private String convertRegString(String input) {
+		if (input.matches("\\$T[0-9]+")) {
+			try {
+				int temp = Integer.parseInt(input.substring(2, 3)) ;
+				return "r" + (temp - 1);
+			} catch (Exception e) {
+				return "FAILED TO PARSE INT FROM: " + input + "\n";
+			}
+		} else {
+			return input;
+		}
 	}
 }
